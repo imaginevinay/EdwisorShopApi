08115896b11db9146994bc7a534d7b591cc28346
Vinay Varshney <vinay.varshney28@gmail.com>
2018-05-19 17:54:25 +0530

    Initial commit

---
+++ apiDocs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Edwisor Shop Api",
+  "version": "0.0.1",
+  "description": "Documentation for Edwisor Shop Api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-19T11:33:20.620Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
